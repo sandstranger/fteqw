7aff100d076f57e0e887cb43bf6b5baafad0bd0b
zhaodengke <beyondk2000@gmail.com>
2025-02-27 12:53:04 +0800

    FTEQW: fix q2

--- engine/server/svq2_game.c
+++ engine/server/svq2_game.c
@@ -91,6 +91,15 @@ void *SVQ2_GetGameAPI (void *parms)
 					continue;
 				Q_snprintfz(name, sizeof(name), "%slibgame_%s"ARCH_DL_POSTFIX, host_parms.binarydir, gamepath);
 			}
+#ifdef ANDROID //karin: load q2game
+			else if (o == 5)
+			{
+				extern char *Sys_MakeDLLPath(const char *libname, char path[], int max_length);
+				char dllName[MAX_OSPATH];
+				Sys_MakeDLLPath(gamename[o], dllName, MAX_OSPATH);
+				Q_snprintfz(name, sizeof(name), "%s", dllName);
+			}
+#endif
 			else if (*gamename[o] == '/')
 			{	//system path. o.O
 				if (!com_gamedirnativecode.ival)	//just in case they match.
