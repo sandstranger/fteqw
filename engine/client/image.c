glKarin/com.n0n3m4.diii4a

FTEQW: fix plugin load

Q3E/src/main/jni/fteqw/engine/client/image.c
@@ -1517,6 +1517,8 @@ qboolean LibPNG_Init(void)
 			{
 			#ifdef _WIN32
 				"libpng" STRINGIFY(PNG_LIBPNG_VER_DLLNUM)
+#elif defined(__ABDROID__) //karin: libpng.so
+				"libpng",
 			#else
 				//linux...
 				//lsb uses 'libpng12.so' specifically, so make sure that works.

Q3E/src/main/jni/fteqw/engine/client/snd_ov.c
@@ -429,6 +429,12 @@ static qboolean OV_StartDecode(unsigned char *start, unsigned long length, ovdec
 		if (!oggvorbislibrary)
 			Con_Printf("Couldn't load DLL: \"vorbisfile.dll\" or \"libvorbisfile-3\".\n");
 	}
+#elif defined(__ABDROID__) //karin: libvorbisfile.so
+	{
+		oggvorbislibrary = Sys_LoadLibrary("libvorbisfile", funcs);
+		if (!oggvorbislibrary)
+			Con_Printf("Couldn't load library: \"libvorbisfile\".\n");
+	}
 #else
 	{
 		oggvorbislibrary = Sys_LoadLibrary("libvorbisfile.so.3", funcs);

Q3E/src/main/jni/fteqw/plugins/avplug/avdecode.c
@@ -9,6 +9,10 @@ static plugaudiofuncs_t *audiofuncs;
 #include "libswscale/swscale.h"
 #include "libavutil/imgutils.h"
 
+#ifdef _DIII4A //karin: dynamic load ffmpeg
+#include "qffmpeg_inc.h"
+#endif
+
 #define TARGET_FFMPEG (LIBAVFORMAT_VERSION_MICRO >= 100)
 #define HAVE_DECOUPLED_API (LIBAVCODEC_VERSION_MAJOR>57 || (LIBAVCODEC_VERSION_MAJOR==57&&LIBAVCODEC_VERSION_MINOR>=36))
 

Q3E/src/main/jni/fteqw/plugins/avplug/avencode.c
@@ -9,6 +9,10 @@
 #include "libavutil/opt.h"
 #include "libavutil/channel_layout.h"
 
+#ifdef _DIII4A //karin: dynamic load ffmpeg
+#include "qffmpeg_inc.h"
+#endif
+
 #define TARGET_FFMPEG (LIBAVFORMAT_VERSION_MICRO >= 100)
 
 #if TARGET_FFMPEG

sandstranger/fteqw

engine/client/image.c
@@ -1517,6 +1517,8 @@ qboolean LibPNG_Init(void)
 			{
 			#ifdef _WIN32
 				"libpng" STRINGIFY(PNG_LIBPNG_VER_DLLNUM)
+#elif defined(__ANDROID__) //karin: libpng.so
+				"libpng",
 			#else
 				//linux...
 				//lsb uses 'libpng12.so' specifically, so make sure that works.

engine/client/snd_ov.c
@@ -429,6 +429,12 @@ static qboolean OV_StartDecode(unsigned char *start, unsigned long length, ovdec
 		if (!oggvorbislibrary)
 			Con_Printf("Couldn't load DLL: \"vorbisfile.dll\" or \"libvorbisfile-3\".\n");
 	}
+#elif defined(__ANDROID__) //karin: libvorbisfile.so
+	{
+		oggvorbislibrary = Sys_LoadLibrary("libvorbis", funcs);
+		if (!oggvorbislibrary)
+			Con_Printf("Couldn't load library: \"libvorbis\".\n");
+	}
 #else
 	{
 		oggvorbislibrary = Sys_LoadLibrary("libvorbisfile.so.3", funcs);

plugins/avplug/avdecode.c
@@ -9,6 +9,10 @@ static plugaudiofuncs_t *audiofuncs;
 #include "libswscale/swscale.h"
 #include "libavutil/imgutils.h"
 
+#ifdef ANDROID //karin: dynamic load ffmpeg
+#include "qffmpeg_inc.h"
+#endif
+
 #define TARGET_FFMPEG (LIBAVFORMAT_VERSION_MICRO >= 100)
 #define HAVE_DECOUPLED_API (LIBAVCODEC_VERSION_MAJOR>57 || (LIBAVCODEC_VERSION_MAJOR==57&&LIBAVCODEC_VERSION_MINOR>=36))
 

plugins/avplug/avencode.c
@@ -9,6 +9,10 @@
 #include "libavutil/opt.h"
 #include "libavutil/channel_layout.h"
 
+#ifdef ANDROID //karin: dynamic load ffmpeg
+#include "qffmpeg_inc.h"
+#endif
+
 #define TARGET_FFMPEG (LIBAVFORMAT_VERSION_MICRO >= 100)
 
 #if TARGET_FFMPEG