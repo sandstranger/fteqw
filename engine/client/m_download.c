glKarin/com.n0n3m4.diii4a

FTEQW: fix quake3(using qvm, library has error)

Q3E/src/main/jni/fteqw/engine/client/m_download.c
@@ -589,6 +589,12 @@ static void PM_ValidatePackage(package_t *p)
 					fl = DPF_CACHED;
 					//fixme: skip any archive checks
 				}
+#ifdef _DIII4A //karin: lib*.so on other path
+				if (!pf && (p->flags & DPF_PLUGIN))
+				{
+					pf = FS_OpenVFS(dep->name, "rb", FS_LIBRARYPATH);
+				}
+#endif
 			}
 			else if (dep->dtype == DEP_CACHEFILE)
 			{
@@ -1732,6 +1738,13 @@ void PM_EnumeratePlugins(void (*callback)(const char *name, qboolean blocked))
 
 	for (p = availablepackages; p; p = p->next)
 	{
+#ifdef _DIII4A //karin: TODO: enabled default
+		if(fs_manifest && fs_manifest->installupd)
+		{
+			if(strcmp(p->name, fs_manifest->installupd) == 0)
+				p->flags |= DPF_ENABLED;
+		}
+#endif
 		if ((p->flags & DPF_ENABLED) && (p->flags & DPF_PLUGIN))
 		{
 			for (d = p->deps; d; d = d->next)

Q3E/src/main/jni/fteqw/engine/common/bothdefs.h
@@ -634,6 +634,9 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 	#define ARCH_DL_POSTFIX ".so"
 #endif
 
+#ifdef _DIII4A //karin: remove library name tail
+#define ARCH_CPU_POSTFIX ""
+#endif
 #ifndef ARCH_CPU_POSTFIX
 	#if defined(_M_AMD64) || defined(__amd64__) || defined(__x86_64__)
 		#ifdef __ILP32__

Q3E/src/main/jni/fteqw/engine/common/fs.c
@@ -70,7 +70,11 @@ static char *vidfilenames[] =	//list of filenames to check to see if graphics st
 	"set r_replacemodels " IFMINIMAL("","md3 md5mesh")"\n"	\
 	"set r_glsl_emissive 0\n" /*work around the _glow textures not being meant to glow*/
 /*Q3's ui doesn't like empty model/headmodel/handicap cvars, even if the gamecode copes*/
+#ifdef _DIII4A //karin: error if load *.so, so using *.qvm
+#define Q3CFG "//schemes quake3\n" "set v_gammainverted 0\nset snd_ignorecueloops 1\nsetfl g_gametype 0 s\nset gl_clear 1\nset r_clearcolour 0 0 0\nset com_parseutf8 0\ngl_overbright "FORWEB("0","2")"\nseta model sarge\nseta headmodel sarge\nseta handicap 100\ncom_gamedirnativecode 0\nsv_port "STRINGIFY(PORT_Q3SERVER)"\ncl_defaultport "STRINGIFY(PORT_Q3SERVER)"\ncom_protocolversion 68\n"
+#else
 #define Q3CFG "//schemes quake3\n" "set v_gammainverted 0\nset snd_ignorecueloops 1\nsetfl g_gametype 0 s\nset gl_clear 1\nset r_clearcolour 0 0 0\nset com_parseutf8 0\ngl_overbright "FORWEB("0","2")"\nseta model sarge\nseta headmodel sarge\nseta handicap 100\ncom_gamedirnativecode 1\nsv_port "STRINGIFY(PORT_Q3SERVER)"\ncl_defaultport "STRINGIFY(PORT_Q3SERVER)"\ncom_protocolversion 68\n"
+#endif
 //#define RMQCFG "sv_bigcoords 1\n"
 
 #define HLCFG "plug_load ffmpeg"
@@ -2801,10 +2805,9 @@ static qboolean FS_NativePath(const char *fname, enum fs_relative relativeto, ch
 		break;
 #elif defined(_DIII4A) //karin: load library path
 		{
-			extern char *Sys_MakeDLLPath(const char *libname, char path[], int max_length);
-			char dllName[MAX_OSPATH];
-			Sys_MakeDLLPath(fname, dllName, MAX_OSPATH);
-			nlen = Q_snprintfz(out, outlen, "%s", dllName);
+			extern const char * Sys_DLLInternalPath(void);
+			const char *dllPath = Sys_DLLInternalPath();
+			nlen = Q_snprintfz(out, outlen, "%s/%s", dllPath, fname);
 		}
 		break;
 #else

Q3E/src/main/jni/fteqw/engine/common/qvm.c
@@ -93,6 +93,12 @@ qboolean QVM_LoadDLL(vm_t *vm, const char *name, qboolean binroot, void **vmMain
 	hVM=NULL;
 	*fname = 0;
 
+#ifdef _DIII4A //karin: load libq3*.so
+	if (!hVM && FS_SystemPath(va("%s" ARCH_DL_POSTFIX, name), FS_LIBRARYPATH, fname, sizeof(fname)))
+	{
+		hVM = Sys_LoadLibrary(name, funcs);
+	}
+#endif
 	if (binroot)
 	{
 		//load eg: basedir/module_gamedir_arch.ext

sandstranger/fteqw

engine/client/m_download.c
@@ -589,6 +589,12 @@ static void PM_ValidatePackage(package_t *p)
 					fl = DPF_CACHED;
 					//fixme: skip any archive checks
 				}
+#ifdef ANDROID //karin: lib*.so on other path
+				if (!pf && (p->flags & DPF_PLUGIN))
+				{
+					pf = FS_OpenVFS(dep->name, "rb", FS_LIBRARYPATH);
+				}
+#endif
 			}
 			else if (dep->dtype == DEP_CACHEFILE)
 			{
@@ -1732,6 +1738,13 @@ void PM_EnumeratePlugins(void (*callback)(const char *name, qboolean blocked))
 
 	for (p = availablepackages; p; p = p->next)
 	{
+#ifdef ANDROID //karin: TODO: enabled default
+		if(fs_manifest && fs_manifest->installupd)
+		{
+			if(strcmp(p->name, fs_manifest->installupd) == 0)
+				p->flags |= DPF_ENABLED;
+		}
+#endif
 		if ((p->flags & DPF_ENABLED) && (p->flags & DPF_PLUGIN))
 		{
 			for (d = p->deps; d; d = d->next)

engine/common/bothdefs.h
@@ -630,6 +630,9 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 	#define ARCH_DL_POSTFIX ".so"
 #endif
 
+#ifdef ANDROID //karin: remove library name tail
+#define ARCH_CPU_POSTFIX ""
+#endif
 #ifndef ARCH_CPU_POSTFIX
 	#if defined(_M_AMD64) || defined(__amd64__) || defined(__x86_64__)
 		#ifdef __ILP32__

engine/common/fs.c
@@ -70,7 +70,11 @@ static char *vidfilenames[] =	//list of filenames to check to see if graphics st
 	"set r_replacemodels " IFMINIMAL("","md3 md5mesh")"\n"	\
 	"set r_glsl_emissive 0\n" /*work around the _glow textures not being meant to glow*/
 /*Q3's ui doesn't like empty model/headmodel/handicap cvars, even if the gamecode copes*/
+#ifdef ANDROID //karin: error if load *.so, so using *.qvm
+#define Q3CFG "//schemes quake3\n" "set v_gammainverted 0\nset snd_ignorecueloops 1\nsetfl g_gametype 0 s\nset gl_clear 1\nset r_clearcolour 0 0 0\nset com_parseutf8 0\ngl_overbright "FORWEB("0","2")"\nseta model sarge\nseta headmodel sarge\nseta handicap 100\ncom_gamedirnativecode 0\nsv_port "STRINGIFY(PORT_Q3SERVER)"\ncl_defaultport "STRINGIFY(PORT_Q3SERVER)"\ncom_protocolversion 68\n"
+#else
 #define Q3CFG "//schemes quake3\n" "set v_gammainverted 0\nset snd_ignorecueloops 1\nsetfl g_gametype 0 s\nset gl_clear 1\nset r_clearcolour 0 0 0\nset com_parseutf8 0\ngl_overbright "FORWEB("0","2")"\nseta model sarge\nseta headmodel sarge\nseta handicap 100\ncom_gamedirnativecode 1\nsv_port "STRINGIFY(PORT_Q3SERVER)"\ncl_defaultport "STRINGIFY(PORT_Q3SERVER)"\ncom_protocolversion 68\n"
+#endif
 //#define RMQCFG "sv_bigcoords 1\n"
 
 #define HLCFG "plug_load ffmpeg\n"
@@ -2719,29 +2723,8 @@ static vfsfile_t *VFS_Filter(const char *filename, vfsfile_t *handle)
 }
 
 #if ANDROID
-char * Sys_MakeDLLPath(const char *libname, char path[], int max_length)
-{
-#undef snprintf
-#define HARM_MAX_OSPATH 1024
-    char dllName[HARM_MAX_OSPATH];
-    memset(dllName, 0, HARM_MAX_OSPATH);
-    size_t libnameLength = strlen(libname);
-
-    if(libnameLength >= 3 && (libname[0] != 'l' || libname[1] != 'i' || libname[2] != 'b'))
-        snprintf(dllName, HARM_MAX_OSPATH - 1, "lib");
-    snprintf(dllName + strlen(dllName), HARM_MAX_OSPATH - 1 - strlen(dllName), "%s", libname);
-
-    if(libnameLength >= 3 && (libname[libnameLength - 3] != '.' || libname[libnameLength - 2] != 's' || libname[libnameLength - 1] != 'o'))
-        snprintf(dllName + strlen(dllName), HARM_MAX_OSPATH - 1 - strlen(dllName), ".so");
-
-    memset(path, 0, max_length);
-    const char * dllDefaultPath = getenv("DLL_DEFAULT_PATH");
-    if(dllDefaultPath && dllDefaultPath[0])
-        snprintf(path, max_length - 1, "%s/%s", dllDefaultPath, dllName);
-    else
-        snprintf(path, max_length - 1, "%s", dllName);
-    return path;
-#undef HARM_MAX_OSPATH
+char * Sys_DLLInternalPath() {
+    return getenv("DLL_DEFAULT_PATH");
 }
 #endif
 
@@ -2834,9 +2817,8 @@ static qboolean FS_NativePath(const char *fname, enum fs_relative relativeto, ch
 		break;
 #elif defined(ANDROID) //karin: load library path
 		{
-			char dllName[MAX_OSPATH];
-			Sys_MakeDLLPath(fname, dllName, MAX_OSPATH);
-			nlen = Q_snprintfz(out, outlen, "%s", dllName);
+			const char *dllPath = Sys_DLLInternalPath();
+			nlen = Q_snprintfz(out, outlen, "%s/%s", dllPath, fname);
 		}
 		break;
 #else

engine/common/qvm.c
@@ -93,6 +93,12 @@ qboolean QVM_LoadDLL(vm_t *vm, const char *name, qboolean binroot, void **vmMain
 	hVM=NULL;
 	*fname = 0;
 
+#ifdef ANDROID //karin: load libq3*.so
+	if (!hVM && FS_SystemPath(va("%s" ARCH_DL_POSTFIX, name), FS_LIBRARYPATH, fname, sizeof(fname)))
+	{
+		hVM = Sys_LoadLibrary(name, funcs);
+	}
+#endif
 	if (binroot)
 	{
 		//load eg: basedir/module_gamedir_arch.ext