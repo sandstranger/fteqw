54d2cf3fc30c71c6b2e24e5c97324f03539fd68b
sandstranger <sandstranger@gmail.com>
2026-01-09 13:27:39 +0400

    fix linking static png

--- engine/client/image.c
+++ engine/client/image.c
@@ -1518,7 +1518,7 @@ qboolean LibPNG_Init(void)
 			#ifdef _WIN32
 				"libpng" STRINGIFY(PNG_LIBPNG_VER_DLLNUM)
 #elif defined(__ANDROID__) //karin: libpng.so
-				"libpng",
+				"libpng16",
 			#else
 				//linux...
 				//lsb uses 'libpng12.so' specifically, so make sure that works.
