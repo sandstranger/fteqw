d93c5e12525bb399e03512d7f696f3f4fd6deb8d
sandstranger <sandstranger@gmail.com>
2026-01-12 02:06:36 +0400

    remove render overriding

--- engine/client/renderer.c
+++ engine/client/renderer.c
@@ -1084,12 +1084,8 @@ void Renderer_Start(void)
 	}
 	if (!currentrendererstate.renderer)
 	{
-#ifndef ANDROID
         //we still failed. Try again, but use the default renderer.
 		Cvar_Set(&vid_renderer, "");
-#else
-        Cvar_Set(&vid_renderer, getenv("RENDER_TYPE"));
-#endif
 		Cmd_ExecuteString("vid_restart\n", RESTRICT_LOCAL);
 	}
 	if (!currentrendererstate.renderer)
