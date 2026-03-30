5c353627a7b4078d7921e63a5d54d8f5b0838a84
sandstranger <sandstranger@gmail.com>
2026-03-30 23:25:29 +0400

    fix formatting

--- engine/common/cmd.c
+++ engine/common/cmd.c
@@ -655,7 +655,6 @@ void Cmd_StuffCmds (void)
 
 #if defined(HAVE_LEGACY) && defined(HAVE_CLIENT)
 static const char *replacementq1binds =
-    "%s\n"
 	"unbindall\n"
 	"bind		`			toggleconsole\n"
 	"bind		w			+forward\n"
@@ -1023,8 +1022,8 @@ static void Cmd_Exec_f (void)
         RemoveLine("alias zoom_in",s);
         RemoveLine("alias zoom_out",s);
         if (activeGame == MGT_QUAKE1 || (l == 1914 && CalcHashInt(&hash_md4, f, l) == 0x2d7b72b9)) {
-            s = va( replacementq1binds, s);
-        }
+            s = va("%s\n%s", s, replacementq1binds);
+		}
 #ifdef HEXEN2
 		else if (activeGame == MGT_HEXEN2 || (l == 1875 && CalcHashInt(&hash_md4, f, l) == 0x27b4d813))
 		{	//hexen2 has weird stuff in there. just give it wasd.
