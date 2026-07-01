303dff53be2355d895115df6b4840cf3dfee085e
sandstranger <sandstranger@gmail.com>
2026-07-01 12:57:57 +0400

    fix: always enable 24 bit gl depth for Android, adreno really hates 16 bit gl depth in this legacy engine

--- engine/gl/gl_vidsdl.c
+++ engine/gl/gl_vidsdl.c
@@ -454,7 +454,7 @@ static qboolean SDLVID_Init (rendererstate_t *info, unsigned char *palette, r_qr
 			SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 0);
 			SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 0);
 		}
-		SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 16);
+		SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
 		SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 
 		if (info->stereo)
